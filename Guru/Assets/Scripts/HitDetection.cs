1ff40e816d560299f8991d17204f9c0adf6b37f5
unknown <orthogonaltrajectory@gmail.com>
2019-02-17 19:28:57 -0900

    HitRing outputs sword location

---
+++ Guru/Assets/Scripts/HitDetection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(gameObject.name + " detected " + other.name);
+    }
+}
