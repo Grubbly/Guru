38b76ad0eaedb4415e6e187da5b1293523a143ec
unknown <orthogonaltrajectory@gmail.com>
2019-02-17 20:28:30 -0900

    Horizontal block on space

---
+++ Guru/Assets/HardBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardBlock : MonoBehaviour
+{
+    Animator animator;
+    int horizontalBlockHash = Animator.StringToHash("blockHorizontal");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void block(string id) {
+        if(id == "horizontal")
+            animator.SetTrigger(horizontalBlockHash);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger(horizontalBlockHash);
+        }
+    }
+}
