a2ff26c1495409f332929c7e29ae9567518f8c60
unknown <orthogonaltrajectory@gmail.com>
2019-02-17 21:01:09 -0900

    Hard coded horizontal slash block

--- Guru/Assets/Scripts/HitDetection.cs
+++ Guru/Assets/Scripts/HitDetection.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class HitDetection : MonoBehaviour
 {
+    private UserMotion userMotion;
+
+    private void Awake() {
+        userMotion = gameObject.GetComponentInParent<UserMotion>();
+        Debug.Log(userMotion.enter);
+    }
+
     private void OnTriggerEnter(Collider other) {
-        Debug.Log(gameObject.name + " detected " + other.name);
+        if(userMotion.enter == "enter") {
+            userMotion.enter = gameObject.name;
+            Debug.Log("enter set to " + userMotion.enter);
+        } else if(userMotion.exit == "exit") {
+            userMotion.exit = gameObject.name;
+            Debug.Log("exit set to " + userMotion.exit);
+        }
     }
 }
---
+++ Guru/Assets/UserMotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMotion : MonoBehaviour
+{
+    public GameObject AILeftArm;
+    private HardBlock hardBlock;
+    public string enter; 
+    public string exit;
+
+    private void Awake() {
+        enter = "enter";
+        exit = "exit";
+        hardBlock = AILeftArm.GetComponent<HardBlock>();
+    }
+
+    private void Update() {
+        if(enter != "enter" && exit != "exit") {
+            if((enter=="East" && exit=="West") || (enter=="West" && exit=="East"))
+                hardBlock.block("horizontal");
+
+            enter = "enter";
+            exit = "exit";
+            Debug.Log("UserMotion entry and exit points reset");
+        }
+    }
+    
+}
