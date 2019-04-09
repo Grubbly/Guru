a709c4050d5bb574ebe1a09f791f87c8be26af28
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2019-04-09 14:13:47 -0800

    Cardinal direction combinations

--- Guru/Assets/Scripts/Brain.cs
+++ Guru/Assets/Scripts/Brain.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Brain : MonoBehaviour
 {
-    int DNALength = 2;
+    int DNALength = 4;
     public DNA dna;
-    public Transform playerSwordPosition;
+    public Transform playerSwordTransform;
     public GameObject AISword;
-    bool seeWall = true;
     public float MAXHP = 100;
-    public float damageTaken = 0;
-    bool alive = true; 
+    public int damageTaken = 0;
+    bool alive = true;
+    bool swordPositionMoved = true;
+    private Vector3 previousSwordPosition;
 
-    private enum Direction {North, East, South, West};
+    public enum Direction {North, East, South, West};
+    public Direction verticalSwordDirection;
+    public Direction horizontalSwordDirection;
 
     private void Awake() {
-        playerSwordPosition = GameObject.Find("PlayerSword").GetComponent<Transform>();
+        playerSwordTransform = GameObject.Find("PlayerSword").GetComponent<Transform>();
+        previousSwordPosition = playerSwordTransform.position;
     }
 
     public void Init() {
@@ -25,29 +29,65 @@ public class Brain : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "dead") {
-            damageTaken = MAXHP;
-            alive = false;
+            damageTaken += 1;
+
+            if(damageTaken == MAXHP) 
+                alive = false;
         }
     }
 
     private void Update() {
         if(!alive) return;
 
-        if(playerSwordPosition.position.y > 2)
+        swordPositionMoved = playerSwordTransform.position != previousSwordPosition;
+
+        if(swordPositionMoved)
         {
-            Debug.Log(playerSwordPosition.position.y);
+            previousSwordPosition = playerSwordTransform.position;
+        }
+
+
+        if (playerSwordTransform.position.y > 0.6)
+        {
+            verticalSwordDirection = Direction.North;
+        }
+        else
+        {
+            verticalSwordDirection = Direction.South;
+        }
+
+        if (playerSwordTransform.position.x > 0)
+        {
+            horizontalSwordDirection = Direction.West;
+        }
+        else
+        {
+            horizontalSwordDirection = Direction.East;
         }
     }
 
     private void FixedUpdate() {
         if(!alive) return;
 
-        float rotateRate = 0;
+        float verticalRotateRate = 0;
+        float horizontalRotateRate = 0;
         // float travelSpeed = dna.GetGene(0);
 
-        rotateRate = dna.GetGene(1);
+        if (swordPositionMoved)
+        {
+            if (verticalSwordDirection == Direction.North)
+                verticalRotateRate = dna.GetGene(0);
+            else
+                verticalRotateRate = dna.GetGene(1);
+
+            if (horizontalSwordDirection == Direction.East)
+                horizontalRotateRate = dna.GetGene(2);
+            else
+                horizontalRotateRate = dna.GetGene(3);
+        }
+            
         
         // this.transform.Translate(0,0,travelSpeed * 0.001f);
-        AISword.transform.Rotate(0,rotateRate,0);
+        AISword.transform.Rotate(horizontalRotateRate,verticalRotateRate,0);
     }
 }
