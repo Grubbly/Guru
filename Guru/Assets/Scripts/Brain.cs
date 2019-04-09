9b81d57bb3049e11d0841f2dec4e41391c719357
unknown <orthogonaltrajectory@gmail.com>
2019-04-09 12:50:49 -0800

    Brain, pop manager, and dna scripts

---
+++ Guru/Assets/Scripts/Brain.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    int DNALength = 2;
+    public DNA dna;
+    public Transform playerSwordPosition;
+    public GameObject AISword;
+    bool seeWall = true;
+    public float MAXHP = 100;
+    public float damageTaken = 0;
+    bool alive = true; 
+
+    private enum Direction {North, East, South, West};
+
+    private void Awake() {
+        playerSwordPosition = GameObject.Find("PlayerSword").GetComponent<Transform>();
+    }
+
+    public void Init() {
+        dna = new DNA(DNALength, 360);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "dead") {
+            damageTaken = MAXHP;
+            alive = false;
+        }
+    }
+
+    private void Update() {
+        if(!alive) return;
+
+        if(playerSwordPosition.position.y > 2)
+        {
+            Debug.Log(playerSwordPosition.position.y);
+        }
+    }
+
+    private void FixedUpdate() {
+        if(!alive) return;
+
+        float rotateRate = 0;
+        // float travelSpeed = dna.GetGene(0);
+
+        rotateRate = dna.GetGene(1);
+        
+        // this.transform.Translate(0,0,travelSpeed * 0.001f);
+        AISword.transform.Rotate(0,rotateRate,0);
+    }
+}
---
+++ Guru/Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public GameObject startingPos;
+    public int populationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0f;
+    public float trialTime = 10;
+    public float timeWalkingWeight = 5f;
+    int generation = 1;
+
+    GUIStyle gui = new GUIStyle();
+
+    private void OnGUI() {
+        gui.fontSize = 30;
+        gui.normal.textColor = Color.red;
+        GUI.BeginGroup (new Rect(10,10,250,150));
+        GUI.Box(new Rect(0,0,140,140), "Stats", gui);
+        GUI.Label(new Rect(10,25,200,30), "Gen: " + generation, gui);
+        GUI.Label(new Rect(10,50,200,30), string.Format("Time: {0:0.00}", elapsed), gui);
+        GUI.Label(new Rect(10,75,200,30), "Population: " + population.Count, gui);
+        GUI.EndGroup();
+    }
+
+    private void Start() {
+        for(int i = 0; i < populationSize; i++) {
+            GameObject bot = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+            bot.GetComponent<Brain>().Init();
+            population.Add(bot);
+        }
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2) {
+        GameObject offspring = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+        Brain brain = offspring.GetComponent<Brain>();
+
+        if(Random.Range(0,100) == 1) {
+            brain.Init();
+            brain.dna.Mutate();
+        } else {
+            brain.Init();
+            brain.dna.Crossover(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+        }
+
+        return offspring;
+    }
+
+    void Selection() {
+        List<GameObject> sortedList = population.OrderBy(o => (o.GetComponent<Brain>().damageTaken)).ToList();
+        
+        population.Clear();
+
+        for(int i = 0; i < (int) (sortedList.Count/2f)-1; i++) {
+            population.Add(Breed(sortedList[i], sortedList[i+1]));
+            population.Add(Breed(sortedList[i+1], sortedList[i]));
+        }
+
+        foreach(GameObject bot in sortedList) {
+            Destroy(bot);
+        }
+
+        generation++;
+    }
+
+    private void Update() {
+        elapsed += Time.deltaTime;
+        if(elapsed >= trialTime) {
+            Selection();
+            elapsed = 0;
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
